6f2a80b5a138d1d68cc2f2ec2bf8584e49ab5fb3
Robert Di Pardo <dipardo.r@gmail.com>
2022-01-10 20:49:44 -0500

    Update version

--- rplugin/pythonx/cpywrite/__init__.py
+++ rplugin/pythonx/cpywrite/__init__.py
@@ -28,7 +28,7 @@ cpywrite
                      ```````
 """
 
-__version__ = '0.7.0-pre'
+__version__ = '0.7.0'
 __authors__ = ['Robert Di Pardo']
 __url__ = 'https://github.com/rdipardo/vim-cpywrite'
 __license__ = 'MIT'
