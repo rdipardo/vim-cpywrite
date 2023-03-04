rdipardo/vim-cpywrite

commit ee45716a3d5dc3774340f5a437380904429ad71b

Add ftplugin for Java classes

plugin/cpywrite/main.py
@@ -5,11 +5,12 @@ Buffer manipulation functions
 """
 from __future__ import print_function
 import sys
+import os
 import vim
-from re import match
+from re import match, sub
 for path in vim.eval('globpath(&rtp,"rplugin/pythonx",1)').split('\n'):
     sys.path.append(path)
-from cpywrite import Generator
+from cpywrite.generator import Generator, _get_source_author
 
 
 def prepend():
@@ -27,12 +28,12 @@ def prepend():
                 generator.set_file_props(filename.replace('.pl', '.p'), filetype)
                 generator.out_file = filename
 
-            _write_header(generator, vim.current.buffer, filetype)
+            _write_header(generator, vim.current.buffer, filetype, filename)
         except (ValueError, vim.error) as exc:
             print(str(exc))
             return
 
-def _write_header(writer, curr_buffer, filetype):
+def _write_header(writer, curr_buffer, filetype, filename):
     """Write the license header"""
     try:
         old_row, old_col = vim.current.window.cursor
@@ -41,6 +42,7 @@ def _write_header(writer, curr_buffer, filetype):
         exclude_file_name = _get_option_value('g:cpywrite#hide_filename')
         allow_anonymous = _get_option_value('g:cpywrite#no_anonymous')
         preserve_shebangs = _get_option_value('g:cpywrite#preserve_shebangs')
+        include_javadoc = _get_option_value('g:cpywrite#java#add_class_doc')
         header = writer.fetch_license_header(use_text_as_header,
                                              machine_readable,
                                              exclude_file_name,
@@ -75,6 +77,38 @@ def _write_header(writer, curr_buffer, filetype):
                     # https://docs.rubocop.org/rubocop/cops_lint.html#lintscriptpermission
                     to_skip = (to_skip + 1) if filetype != 'ruby' else to_skip
 
+                if include_javadoc:
+                    class_name, _ = os.path.splitext(filename)
+
+                    if curr_line.find(class_name) > -1:
+                        class_doc = idx
+                        docline = curr_buffer[class_doc]
+
+                        while class_doc > 0 and docline.find('/**') == -1:
+                            class_doc -= 1
+                            docline = curr_buffer[class_doc]
+
+                        # insert tag into existing doc comment only
+                        if docline.find('/**') > -1:
+                            class_doc += 1
+                            docline = curr_buffer[class_doc]
+
+                            while class_doc < len(curr_buffer) and \
+                                    not match(r'^\s*.*\*\/', docline):
+                                # doc line must be blank
+                                if match(r'^\s*\*+\s*$', docline):
+                                    author, email = _get_source_author()
+                                    email = sub(r'\<', '&lt;', sub(r'\>', '&gt;', email))
+                                    curr_buffer[class_doc:class_doc] = \
+                                        [docline, '%s @author %s %s' % (docline, author, email)]
+
+                                    break
+                                else:
+                                    class_doc += 1
+                                    docline = curr_buffer[class_doc]
+
+                        break
+
             if to_trim > 0:
                 del curr_buffer[0:to_trim]
 
@@ -86,6 +120,9 @@ def _write_header(writer, curr_buffer, filetype):
         return
 
 def _get_option_value(vim_var):
+    if not vim.vars.has_key(sub(r'[abglsv]:', '', vim_var)):
+        return False
+
     try:
         val = bool(int(vim.eval(vim_var), 10))
     except ValueError:

.github/workflows/ci.yml
@@ -26,7 +26,7 @@ jobs:
         pip install --upgrade pip
         pip install vim-vint
     - name: Lint
-      run: vint autoload plugin -wt
+      run: vint autoload ftplugin plugin -wt
   build:
     needs: lint
     runs-on: ubuntu-20.04
@@ -67,7 +67,8 @@ jobs:
     - name: Set up plugin test environment
       uses: actions/checkout@v3
       with:
-        repository: junegunn/vader.vim
+        repository: blueyed/vader.vim
+        ref: display-source-with-exceptions
         path: vader.vim
         fetch-depth: 1
 

.gitignore
@@ -2,4 +2,5 @@
 *.log
 /__pycache__/
 .pytest_cache/
+test/src/
 vader.vim/

.vintrc.yml
@@ -3,3 +3,5 @@ cmdargs:
 policies:
   ProhibitAbbreviationOption:
     enabled: false
+  ProhibitCommandRelyOnUser:
+    enabled: false

doc/tags
@@ -19,6 +19,7 @@ cpywrite-requirements	cpywrite.txt	/*cpywrite-requirements*
 cpywrite.txt	cpywrite.txt	/*cpywrite.txt*
 g:cpywrite#default_license	cpywrite.txt	/*g:cpywrite#default_license*
 g:cpywrite#hide_filename	cpywrite.txt	/*g:cpywrite#hide_filename*
+g:cpywrite#java#add_class_doc	cpywrite.txt	/*g:cpywrite#java#add_class_doc*
 g:cpywrite#machine_readable	cpywrite.txt	/*g:cpywrite#machine_readable*
 g:cpywrite#no_anonymous	cpywrite.txt	/*g:cpywrite#no_anonymous*
 g:cpywrite#preserve_shebangs	cpywrite.txt	/*g:cpywrite#preserve_shebangs*

ftplugin/java.vim
@@ -1,0 +1,14 @@
+"" ==========================================================================
+""   This filetype plugin is part of vim-cpywrite
+""
+""   Maintainer: Robert Di Pardo <dipardo.r@gmail.com>
+""   URL:        https://github.com/rdipardo/vim-cpywrite
+""   License:    MIT
+"" ==========================================================================
+
+if exists('b:ftplugin_loaded') | finish | endif
+let b:ftplugin_loaded = 1
+
+if !exists('g:cpywrite#java#add_class_doc')
+    let g:cpywrite#java#add_class_doc = 1
+endif

rplugin/pythonx/cpywrite/generator.py
@@ -104,15 +104,14 @@ class Generator(object): # pylint: disable=R0205
         try:
             out = StringIO()
             year = str(datetime.now())[:4]
-            author, email = _get_source_author()
-            contact = ' <' + email + '>' if email else ''
+            author, contact = _get_source_author()
             # use the tag prefix specified by REUSE
             # https://reuse.software/spec/#comment-headers
             copying = \
                 'Copyright (c) ' \
                 if not cpu_readable \
                 else 'SPDX-FileCopyrightText: '
-            copying += year + ' ' + author + contact
+            copying += year + ' ' + author + ' ' + contact
             copyrightable = \
                 no_anon or cpu_readable or \
                 not in_pub_domain(self.rights.spdx_code)
@@ -129,7 +128,7 @@ class Generator(object): # pylint: disable=R0205
             # This prevents faulty matches with the FSF ZIP code in the GPL v1 and 2
             author_date_re = \
                 r'(?!.*(http).*)[\<\(\[\s%s]((YEAR)%s|[YX]+)[\>\)\]\s,-].+[\>\)\]\w+]' \
-                % (('\d', '|\d{3}') \
+                % ((r'\d', r'|\d{3}') \
                    if self.rights.spdx_code == '0BSD' \
                    else ('', '')) \
                 if not (self.rights.spdx_code.startswith('ECL') or \
@@ -320,6 +319,7 @@ def _get_source_author():
 
     if bool(git_username):
         author = git_username
+        email = '<' + email + '>' if email else ''
 
     return (author, email)
 

test/vader/pyx/test_java_class_doc.vader
@@ -1,0 +1,77 @@
+After(Clear buffers);
+  %bd!
+
+Execute(Test that plugin was loaded);
+  call vader#log('===== Pre-test environment check =====')
+  Assert exists('g:loaded_cpywrite')
+
+Given java (Class doc comment with blank line):
+
+  /**
+   *
+   */
+  public final class ClassDoc {}
+
+Execute(Activate Java filetype);
+  so ftplugin/java.vim
+  w! test/src/ClassDoc.java
+
+Then(Assert that JavaDoc mode has default setting);
+  Assert exists('g:cpywrite#java#add_class_doc')
+  AssertEqual 1, g:cpywrite#java#add_class_doc
+
+Execute(Hide file name);
+  call vader#log('===== Calling :CPYwriteToggleFilename =====')
+  CPYwriteToggleFilename
+
+Then(Assert that file name is now hidden);
+  AssertEqual 1, g:cpywrite#hide_filename
+
+Execute(Add short license to Java file);
+  e! test/src/ClassDoc.java
+  b ClassDoc.java
+  CPYwrite MIT
+
+Then(@author tag inserted where space allows):
+  AssertEqual ' * @author ', getline(9)[:10]
+
+Given java (A short class doc comment):
+
+  /**
+   */
+  public final class ShortDoc {}
+
+Execute(Activate Java filetype);
+  w! test/src/ShortDoc.java
+
+Execute(Add standard license header to Java file);
+  e! test/src/ShortDoc.java
+  b ShortDoc.java
+  CPYwrite MPL-2.0
+
+Then(@author tag *not* inserted):
+  AssertEqual '/**', getline(9)
+  AssertEqual ' */', getline(10)
+
+Given java (Missing class doc comment):
+
+  public final class NoDoc {}
+
+Execute(Activate Java filetype);
+  w! test/src/NoDoc.java
+
+Execute(Add standard license header to Java file);
+  e! test/src/NoDoc.java
+  b NoDoc.java
+  CPYwrite BitTorrent-1.1
+
+Then(@author tag *not* inserted):
+  AssertEqual 0, len(getline(13))
+  AssertEqual 'public final class NoDoc {}', getline(14)
+
+Execute(Restore file name);
+  call vader#log('===== Calling :CPYwriteToggleFilename =====')
+  CPYwriteToggleFilename
+
+Then(Assert that file name is now showing);
+  AssertEqual 0, g:cpywrite#hide_filename

commit 4aa7edfdf69f3b852709c4eff38dbe389727483e

Prefer vim.vars.get() to vim.vars.has_key()
Restores neovim compatibility, broken by ee45716

plugin/cpywrite/main.py
@@ -120,7 +120,7 @@ def _write_header(writer, curr_buffer, filetype, filename):
         return
 
 def _get_option_value(vim_var):
-    if not vim.vars.has_key(sub(r'[abglsv]:', '', vim_var)):
+    if vim.vars.get(sub(r'[abglsv]:', '', vim_var)) is None:
         return False
 
     try: